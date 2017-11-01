225ff74200ffdc69dab7a9fc698becfcc3ce6c81
Hamza Boughaleb <hamza.boughaleb@gmail.com>
2017-11-01 08:41:47 -0400

    implemented users and sessions routing and installed bcrypt gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
+  resources :users
+  resources :sessions
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+  Article.create!(
+    title: Faker::HitchhikersGuideToTheGalaxy.quote,
+    content: Faker::Lorem.paragraph(20, true, 20),
+  )
+end
